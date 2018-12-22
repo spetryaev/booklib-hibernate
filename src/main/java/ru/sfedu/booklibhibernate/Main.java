9a7b33e1837c4bae053a7a30c9781ad53546a82a
Sergey <sergey08rus@gmail.com>
2018-12-22 16:00:09 +0300

    m

--- src/main/java/ru/sfedu/booklibhibernate/Main.java
+++ src/main/java/ru/sfedu/booklibhibernate/Main.java
@@ -1,7 +1,11 @@
 package ru.sfedu.booklibhibernate;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private static final Logger log = Logger.getLogger(Main.class);
+    
     public static void main(String[] args) {
         System.out.println("\ntest hello\n");
+        log.info("testlog");
     }
 }
